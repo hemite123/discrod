bc8c62f1652b05aaa3cefdbf83b5b42cac65cfa2
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-03 19:24:28 +0700

    asd

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def pokemondata():
         response1 = requests.get(namepokemon["url"])
         djson = response1.json()
         for pokeid in djson:
-            responseapi = requests.get("https://pokeapi.glitch.me/v1/pokemon/"+pokeid["id"],headers=headers)
+            responseapi = requests.get("https://pokeapi.glitch.me/v1/pokemon/"+str(pokeid["id"]),headers=headers)
             datajson = responseapi.json()
             for info in datajson:
                 if len(info) > 1:
