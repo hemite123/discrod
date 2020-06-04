8f501d891170b0777e47e2ff177bedf6fce2cbdc
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-04 20:00:29 +0700

    DATABASE HOSTING

--- db.py
+++ db.py
@@ -1,50 +1,26 @@
-import pymysql
+import requests
+import json
 
-connection = pymysql.connect(host="sql12.freemysqlhosting.net",user="sql12344778",passwd="KP46SK96Gv",database="sql12344778")
-cursor = connection.cursor()
 
 def GetBalance(self, id):
-    retrive = "SELECT balance FROM user WHERE user_id = " + str(id)
-    cursor.execute(retrive)
-    rows = cursor.fetchall()
-    return rows
-    connection.commit()
-
+    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata/"+str(id),headers="Content-Type:application/json")
+    response = getdata.json()
+    balance = response["balance"]
+    return balance
 def InputUser(self,id,name):
-    insert = "INSERT INTO user(user_id,nickname,balance) VALUES('"+ str(id) + "','" + str(name)  +"',0);"
-    cursor.execute(insert)
-    connection.commit()
+    data = {"user_id":id,"name":name,"balance":0,"datapost":"insert"}
+    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data)
 
 def CheckUser(self,id,act):
-    retrive = "SELECT * FROM user WHERE user_id = " + str(id)
-    cursor.execute(retrive)
-    rows = cursor.fetchall()
+    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata/"+str(id),headers="Content-Type:application/json")
+    response = getdata.json()
     if act == "rcount":
-        return cursor.rowcount
+        return len(response)
     else:
-        return rows
-    connection.commit()        
+        return response
 
 def UpdateUserPokemon(self,id,pokename):
-    updateData = "UPDATE user SET pokestart='"+str(pokename)+"'WHERE user_id=" + str(id)
-    cursor.execute(updateData)
-    connection.commit()
-
-def GetPokedex(self,id):
-    getpokedex = "SELECT * FROM pokedex WHERE user_id=" + str(id)
-    cursor.execute(getpokedex)
-    rows = cursor.fetchall()
-    return rows
-    connection.commit()
-
-def InsertPokeDex(self,number, id,pokemonname,price):
-    inserq = f"INSERT INTO pokedex(number,user_id,pokemonname,price,claim) VALUES({number},{str(id)},{str(pokemonname)},{price},0);"
-    cursor.execute(inserq)
-    connection.commit()
-
-def InsertListPokemon(self,number,id,pokemonname,level):
-    inserq = f"INSERT INTO listpokemon(number,user_id,pokemonname,level,curexp,stat) VALUES({number},{str(id)},{str(pokemonname)},{level},0,{level});"
-    cursor.execute(inserq)
-    connection.commit()
+    data = {"user_id":id,"pokestart":pokename,"datapost":"update"}
+    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data,headers="Content-Type:application/json")
     
 
