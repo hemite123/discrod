fc971be24b99dc46790b93df9cdd078078d4c570
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-04 13:04:33 +0700

    Cache Request

---
+++ getdata.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import os
+
+for i in range(1,500):
+    response = requests.get("https://pokeapi.glitch.me/v1/pokemon/"+str(i))
+    data = response.json()
+    filepath = 'pokemon.json'
+    print(i)
+
+    try:
+        if os.stat(filepath).st_size == 0:
+            with open('pokemon.json','w') as d:
+                json.dump(data,d)   
+        else:
+            with open('pokemon.json') as f:
+                jsonload = json.load(f)
+            jsonload.append(data)
+    
+            with open('pokemon.json','w') as d:
+                json.dump(jsonload,d)
+    except Exception as e:
+        print(e)
+        
