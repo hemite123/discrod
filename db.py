1095b3382dac045ae2bd5211cb2ae3efcef29921
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-04 20:31:36 +0700

    data

--- db.py
+++ db.py
@@ -3,16 +3,16 @@ import json
 
 
 def GetBalance(self, id):
-    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata/"+str(id),headers="{"Content-Type":"application/json"}")
+    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata/"+str(id),headers={"Content-Type":"application/json"})
     response = getdata.json()
     balance = response["balance"]
     return balance
 def InputUser(self,id,name):
     data = {"user_id":id,"name":name,"balance":0,"datapost":"insert"}
-    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data)
+    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data,headers={"Content-Type":"application/json"})
 
 def CheckUser(self,id,act):
-    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata/"+str(id),headers="{"Content-Type":"application/json"}")
+    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata/"+str(id),headers={"Content-Type":"application/json"})
     response = getdata.json()
     if act == "rcount":
         return len(response)
@@ -21,6 +21,6 @@ def CheckUser(self,id,act):
 
 def UpdateUserPokemon(self,id,pokename):
     data = {"user_id":id,"pokestart":pokename,"datapost":"update"}
-    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data,headers="{"Content-Type":"application/json"}")
+    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data,headers={"Content-Type":"application/json"})
     
 
