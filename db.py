83515b61d9a3a8f084a50f731dfde5338197d4da
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-04 21:47:18 +0700

    asdw

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ def GetBalance(self, id):
 def InputUser(self,id,name):
     data = {"user_id":id,"name":name,"balance":0,"datapost":"insert"}
     senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata",data=data,headers={"Content-Type":"application/json","Content-Type" : "x-www-form-urlencoded"})
-    
+    print(senddata.text)
     
 def CheckUser(self,id,act):
     getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata.php/?user_id="+str(id),headers={"Content-Type":"application/json"})
