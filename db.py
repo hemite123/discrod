3beef120a081eb0ac4feb2a51c562137f9c9db90
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-05 15:38:04 +0700

    catch

--- db.py
+++ db.py
@@ -3,12 +3,12 @@ import json
 
 
 def GetBalance(self, id):
-    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata.php/?user_id="+str(id),headers={"Content-Type":"application/json"})
+    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata.php/?user_id="+str(id)+"&action=data",headers={"Content-Type":"application/json"})
     response = getdata.json()
     balance = response["balance"]
     return balance
 def InputUser(self,id,name):
-    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata.php",json={"user_id":id,"name":name,"balance":0,"datapost":"insert"},headers={"Content-Type":"application/json",'Accept': 'text/plain'})
+    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata.php",json={"user_id":id,"name":name,"datapost":"insert"},headers={"Content-Type":"application/json",'Accept': 'text/plain'})
     print(senddata.text)
     
 def CheckUser(self,id,act):
@@ -22,5 +22,13 @@ def CheckUser(self,id,act):
 
 def UpdateUserPokemon(self,id,pokename):
     senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata.php",json={"user_id":id,"pokestart":pokename,"datapost":"update"},headers={"Content-Type":"application/json",'Accept': 'text/plain'})
-    
+    print(senddata.text)
 
+def InsertPokemon(self,id,pokename,level,nomor):
+    senddata = requests.post(url="https://projectdiscord.000webhostapp.com/userdata.php",json={"user_id":id,"pokemon":pokename,"datapost":"newpokemon","nomor":nomor,"level":level},headers={"Content-Type":"application/json",'Accept': 'text/plain'})
+    print(senddata.text)
+
+def ManyPokemon(self,id):
+    getdata = requests.get("https://projectdiscord.000webhostapp.com/userdata.php/?user_id="+str(id)+"&action=manypoke",headers={"Content-Type":"application/json"})
+    response = getdata.json()
+    return response["many"]
