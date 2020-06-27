d3ac84577d763777882b7666b5fead70e145f186
hemite123 <44366436+hemite123@users.noreply.github.com>
2020-06-27 22:46:20 +0700

    tst

--- main.py
+++ main.py
@@ -375,6 +375,7 @@ async def on_message(message):
 async def on_ready():
     print(f"{bot.user.name} Has Connect To Server")
     client = discord.Client()
+    print(len(client.get_all_channels()))
     for chan in client.get_all_channels():
         print(chan)
     
